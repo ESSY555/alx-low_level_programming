06f3f070157a48b5cf55502554ce519510264ad8
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-03 14:33:19 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
