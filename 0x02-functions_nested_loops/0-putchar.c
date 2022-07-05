3f3964d37f974d13140df9c15663ea244a750eee
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-05 23:38:24 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,36 @@
 _putchar
+#include "holberton.h"
+
+/**
+
+ * main - Entry block
+
+ * Description: prints holberton on new line
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+	char c[10] = "Holberton";
+
+	int i = 0;
+
+	while (i < 9)
+
+	{
+
+		_putchar(c[i]);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
