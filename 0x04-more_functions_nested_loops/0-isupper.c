c97123400fa3237146d166cf66d9ed598b9a8a36
ESSY555 <adewumiisrael66@gmail.com>
2022-07-08 14:55:13 -0700

    this is great

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,4 @@
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+
 #include "main.h"
 
 /**
@@ -20,6 +9,7 @@ void print_number(int n);
 
 int _isupper(int c)
 {
-    return (c >= 65 && c <= 90);
+
+return (c >= 65 && c <= 90);
 
 }
