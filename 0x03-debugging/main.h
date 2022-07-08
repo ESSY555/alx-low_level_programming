e43ab0c12d4cb986b413f8679c47a61cc57f8695
ESSY555 <adewumiisrael66@gmail.com>
2022-07-08 12:58:32 -0700

    main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main.h_H
+#define main.h_H
+
+#include <stdio.h>
+
+void positive_or_negative(int);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* main.h_H */
