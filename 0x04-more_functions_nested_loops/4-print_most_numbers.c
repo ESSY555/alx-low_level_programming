194eb6b9cfc2a7af23348c3fc18485b03aeb32f4
ESSY555 <adewumiisrael66@gmail.com>
2022-07-08 15:37:03 -0700

    rt

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,17 @@
 
 void print_most_numbers(void)
 {
-    char n = 0;
+char n = 0;
 
-    while (n <= 9)
-    {
-        if (n != 2 && n != 4)
-        {
-            _putchar('0' + n);
-        }
-
-        n++;
-    }
-
-    _putchar('\n');
+while (n <= 9)
+{
+if (n != 2 && n != 4)
+{
+_putchar('0' + n);
+}
+n++;
+}
+_putchar('\n');
 
 }
+xx
