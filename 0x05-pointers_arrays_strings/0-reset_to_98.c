37019aaec4c9c0ea4f1d066c8150ac8d42bdb406
ESSY555 <adewumiisrael66@gmail.com>
2022-07-12 12:53:10 -0700

    tgus

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,3 +10,15 @@ void reset_to_98(int *n)
 {
    *n = 98;
 }
+#include "holberton.h"
+
+/**
+ * reset_to_98 - reset number to 98
+ * @n: pointer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
