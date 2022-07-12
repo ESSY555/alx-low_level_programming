e73adc97e8445c596a8ca6c84f7bb4d7d8496f89
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-12 22:00:56 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,8 +5,8 @@
  * @dest: char type string
  * @src: char type string
  * Description: Copy the string pointed to by pointer  to
-* the buffer pointed to by 
-* Return: Pointer to 
+ * the buffer pointed to by 
+ * Return:Pointer to 
  */
 
 char *_strcpy(char *dest, char *src)
