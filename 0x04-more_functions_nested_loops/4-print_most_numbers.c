ab1746391d1a297604bf2270918025ddb6c12223
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-13 07:36:13 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,25 +12,25 @@ void print_most_numbers(void)
 
 {
 
-    char n = 0;
+char n = 0;
 
 while (n <= 9)
 
-    {
+{
 
-        if (n != 2 && n != 4)
+if (n != 2 && n != 4)
 
-        {
+{
 
-            _putchar('0' + n);
+_putchar('0' + n);
 
-        }
+}
 
-        n++;
+n++;
 
-    }
+}
 
-    _putchar('\n');
+_putchar('\n');
 
 }
 
