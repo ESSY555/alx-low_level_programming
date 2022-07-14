4ea8a282353936b63c95a97302b912ca781d0eb6
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-14 13:14:22 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,28 +7,28 @@
 
 void more_numbers(void)
 {
-    char n, c;
-    int i = 0;
+char n, c;
+int i = 0;
 
 
-    while (i < 10)
-    {
+while (i < 10)
+{
         
-        for (n = 0; n <= 14; n++)
+for (n = 0; n <= 14; n++)
             
-        {
-            c = n;
-            if (n > 9)
-            {
-                _putchar('1');
-                c = n % 10;
-            }
+{
+c = n;
+if (n > 9)
+{
+_putchar('1');
+c = n % 10;
+}
 
-            _putchar('0' + c);
-        }
+_putchar('0' + c);
+}
 
-        _putchar('\n');
-        i++;
-    }
+_putchar('\n');
+i++;
+}
 
 }
