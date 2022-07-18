a9e5b91f9197bf915f9e1eaa476d0ea505db2671
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-18 21:38:37 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
  * _strpbrk - Search a string for any of a set of bytes.
  * @s: string
  * @accept: string to match
- * Return: Pointer to the byte in s that matches one of the bytes in 
+ * Return: Pointer to the byte in that matches one of the bytes in 
  * or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
