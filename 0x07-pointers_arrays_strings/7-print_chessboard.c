b5092d5915347d1b799fb7abac61909c278da2c2
ESSY555 <adewumiisrael66@gmail.com>
2022-07-18 14:21:10 -0700

    fin

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -18,3 +18,13 @@ _putchar(a[i][j]);
 _putchar('\n');
 }
 }
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
