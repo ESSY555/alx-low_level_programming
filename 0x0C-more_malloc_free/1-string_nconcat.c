a38090da743fdf2cd46667827e6c9f1124947427
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2022-07-28 20:28:19 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,20 +11,20 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *str;
-    unsigned int i, j, s1_length, s2_length;
+char *str;
+unsigned int i, j, s1_length, s2_length;
 
 /*Check if the strings passed are null*/
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 /*Compute the length of the strings*/
 
-    for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-        ;
+for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+;
 
-    for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 ;
 /*Memory reservation-for case 1 & 2.*/
 str = malloc(s1_length + n + 1);
