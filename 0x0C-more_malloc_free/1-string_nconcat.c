8ceb58d9982eeee9c64295482048e4ac9d9459a9
ESSY555 <adewumiisrael66@gmail.com>
2022-07-28 12:22:20 -0700

    second commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,38 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int i, j, s1_length, s2_length;
+    char *str;
+    unsigned int i, j, s1_length, s2_length;
+
 /*Check if the strings passed are null*/
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 /*Compute the length of the strings*/
-for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-;
-for (s2_length = 0;
-s2[s2_length] !=\0; 
-s2_length++);
+
+    for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+        ;
+
+    for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+        ;
 /*Memory reservation-for case 1 & 2.*/
-str = malloc(s1_length + n + 1);
-if (str == NULL)
-{
-return (NULL);
-}
+    str = malloc(s1_length + n + 1);
+    if (str == NULL)
+    {
+        return (NULL);
+    }
 /*Copy first string into str.*/
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
+    for (i = 0; s1[i] != '\0'; i++)
+        str[i] = s1[i];
 /*copy second stringg into str.*/
-for (j = 0; j < n; j++)
-{
-str[i] = s2[j];
-i++;
+    for (j = 0; j < n; j++)
+    {
+        str[i] = s2[j];
+        i++;
+    }
+
+    str[i] = \0;
+    return (str);
 }
-str[i] = '\0';
-return (str);
-}
+
